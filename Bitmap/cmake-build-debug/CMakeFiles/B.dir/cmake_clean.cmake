fb2ef0262800c965f2ceb21cb79810770d50a99a
ThanhTuyen <contact@tuyen.me>
2018-12-20 10:36:20 +0700

    how to analyze a .BMP file

---
+++ Bitmap/cmake-build-debug/CMakeFiles/B.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/B.dir/main.c.o"
+  "B.pdb"
+  "B"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
